1deb17735af03ddbbf40c93959f233a815a4b8b4
Adrieto <parodiadrian3@gmail.com>
2024-09-01 10:28:13 -0300

    conexion en servidor local con mongoDB y pagina inicial con edx

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.get("/",(req, res)=>{
+    res.render('index')
+})
+
+
+app.listen(3900,()=>{
+    console.log("El servidor está funcionando")
+})
